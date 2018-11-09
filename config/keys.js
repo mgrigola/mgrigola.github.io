9518adf33887315c224926ae4ade2a1816593a7b
mgrigola <grigola3@gmail.com>
2018-11-08 19:45:06 -0600

    syncing

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 //mapbox API key unsynced with GitHub
 var configKeys = {
-    mapBoxApiKey: 'pk.eyJ1IjoibWdyaWdvbGEiLCJhIjoiY2pvNjZxa3diMGhpMjNxcGJmeG9wdWdvbiJ9.aZ_CnPXz7eo0Cz585lulXA' //'pk.eyJ1IjoibWdyaWdvbGEiLCJhIjoiY2lubWd0aXFjMHpmdHVrbHkwcTBqNGVnYiJ9.bDdNMfx32jV_AglS1z48Zg'
+    mapBoxApiKey: 'pk.eyJ1IjoibWdyaWdvbGEiLCJhIjoiY2pvNjZxa3diMGhpMjNxcGJmeG9wdWdvbiJ9.aZ_CnPXz7eo0Cz585lulXA' 
 };
